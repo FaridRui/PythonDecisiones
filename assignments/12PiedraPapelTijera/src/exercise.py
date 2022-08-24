13823b380191d643a18efae7c0ebbb442508cf45
FaridRui <110926738+FaridRui@users.noreply.github.com>
2022-08-24 16:33:57 -0500

    Update exercise.py

--- assignments/12PiedraPapelTijera/src/exercise.py
+++ assignments/12PiedraPapelTijera/src/exercise.py
@@ -1,6 +1,21 @@
 def main():
     # Escribe tu código abajo de esta línea
+    ana= input('Ana, dame piedra(a) papel(p) o tijera (t)')
+juan=input('Juan, dame piedra(a) papel(p) o tijera (t)')
+if ana==juan:
+    print('Empate')
+elif ana =='a' and juan=='t':
+    print('Ana gana')
+elif ana == 'p' and juan == 'a':
+    print('Ana gana')
+elif ana== 't' and juan== 'p':
+    print('Ana gana')
+elif ana =='t' and juan=='a':
+    print('Juan gana')
+elif ana =='a' and juan == 'p':
+    print('Juan gana')
+elif ana== 'p' and juan== 't':
+    print('juan gana')
     
-
 if __name__ == '__main__':
     main()
